066f421a67db070987cacb74d0738529da2ff967
rocker <exrobbie@gmail.com>
2023-01-11 15:59:41 +0800

    fixed type issue

--- pages/user/[address]/index.tsx
+++ pages/user/[address]/index.tsx
@@ -48,7 +48,7 @@ export async function getServerSideProps(context: any){
 
 export default function Main({
   pId
-}) {
+}: any) {
   const { account, connectWallet } = useWeb3Context();
   const [showList, setShowList] = useState(false);
   const [handlesList, setHandlesList] = useState<any>([]);
